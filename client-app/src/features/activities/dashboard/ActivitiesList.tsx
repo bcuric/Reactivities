c0d420f0226c92ac66f2559b157f5e3d82b0b62f
bcuric <bcuric@foi.hr>
2020-02-29 23:02:34 +0100

    End of section 7

--- client-app/src/features/activities/dashboard/ActivitiesList.tsx
+++ client-app/src/features/activities/dashboard/ActivitiesList.tsx
@@ -1,51 +1,48 @@
-import React, { SyntheticEvent } from 'react'
-import { Item, Button, Label, Segment } from 'semantic-ui-react'
-import { IActivity } from '../../../app/models/activity'
+import React, { useContext } from 'react';
+import { Item, Button, Label, Segment } from 'semantic-ui-react';
+import { observer } from 'mobx-react-lite';
+import ActivityStore from '../../../app/stores/activityStore';
 
-interface IProps {
-    activities: IActivity[],
-    selectActivity: (id: string) => void; 
-    deleteActivity: (event:SyntheticEvent<HTMLButtonElement>,id: string) => void;
-    submitting:boolean;
-    target: string;
-}
+const ActivityList: React.FC = () => {
+  const activityStore = useContext(ActivityStore);
+  const {activitiesByDate, selectActivity, deleteActivity, submitting, target} = activityStore;
+  return (
+    <Segment clearing>
+      <Item.Group divided>
+        {activitiesByDate.map(activity => (
+          <Item key={activity.id}>
+            <Item.Content>
+              <Item.Header as='a'>{activity.title}</Item.Header>
+              <Item.Meta>{activity.date}</Item.Meta>
+              <Item.Description>
+                <div>{activity.description}</div>
+                <div>
+                  {activity.city}, {activity.venue}
+                </div>
+              </Item.Description>
+              <Item.Extra>
+                <Button
+                  onClick={() => selectActivity(activity.id)}
+                  floated='right'
+                  content='View'
+                  color='blue'
+                />
+                <Button
+                  name={activity.id}
+                  loading={target === activity.id && submitting}
+                  onClick={(e) => deleteActivity(e, activity.id)}
+                  floated='right'
+                  content='Delete'
+                  color='red'
+                />
+                <Label basic content={activity.category} />
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        ))}
+      </Item.Group>
+    </Segment>
+  );
+};
 
-const ActivitiesList: React.FC<IProps> = ({activities, selectActivity,deleteActivity,submitting,target}) => {
-    return (
-        <Segment clearing>
-            <Item.Group divided>
-                {activities.map(activity => (
-                    <Item key={activity.id}>
-                        <Item.Content>
-                            <Item.Header as='a'>{activity.title}</Item.Header>
-                            <Item.Meta>{activity.date}</Item.Meta>
-                            <Item.Description>
-                                <div>{activity.description}</div>
-                                <div>{activity.city}, {activity.venue}</div>
-                            </Item.Description>
-                            <Item.Extra>
-                                <Button 
-                                    onClick={() => selectActivity(activity.id)} 
-                                    floated="right" 
-                                    content="View" 
-                                    color="blue" />
-                                <Button 
-                                    name={activity.id}
-                                    loading={target === activity.id &&submitting}
-                                    onClick={(e) => deleteActivity(e, activity.id)} 
-                                    floated="right" 
-                                    content="Delete" 
-                                    color="red" />
-                                <Label basic content={activity.category}/>
-                            </Item.Extra>
-                        </Item.Content>
-                    </Item>
-                ))}
-                
-            </Item.Group>
-        </Segment>
-       
-    )
-}
-
-export default ActivitiesList
+export default observer(ActivityList);
--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Menu, Container, Button } from 'semantic-ui-react'
+import ActivityStore from '../../app/stores/activityStore';
+import { observer } from 'mobx-react-lite';
 
-interface IProps {
-    openCreateForm: () => void;
-}
 
-const NavBar: React.FC<IProps> = ({openCreateForm}) => {
+const NavBar: React.FC = () => {
+    const activityStore = useContext(ActivityStore);
     return (
         <div>
             <Menu fixed="top" inverted>
@@ -16,7 +16,7 @@ const NavBar: React.FC<IProps> = ({openCreateForm}) => {
                     </Menu.Item>
                     <Menu.Item name='Activities'/>
                     <Menu.Item>
-                        <Button positive onClick={openCreateForm} content="Create Activity" />
+                        <Button positive onClick={activityStore.openCreateForm} content="Create Activity" />
                     </Menu.Item>    
                 </Container>
                 
@@ -25,4 +25,4 @@ const NavBar: React.FC<IProps> = ({openCreateForm}) => {
     )
 }
 
-export default NavBar
+export default observer(NavBar)
