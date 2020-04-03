0498b176810e7e34e36aab133af90cc49a668160
bcuric <bcuric@foi.hr>
2020-04-03 23:01:37 +0200

    Section8

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,31 +1,38 @@
-/* eslint-disable */
-import React, { useEffect, Fragment, useContext } from 'react';
-import { Container } from 'semantic-ui-react'
+import React, { Fragment } from 'react';
+import { Container } from 'semantic-ui-react';
 import NavBar from '../../features/nav/NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponent';
-import ActivityStore from '../stores/activityStore';
 import { observer } from 'mobx-react-lite';
+import { Route, withRouter, RouteComponentProps } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
+import ActivityForm from '../../features/activities/form/ActivityForm';
+import ActivityDetails from '../../features/activities/deteils/ActivityDetails';
+
+const App: React.FC<RouteComponentProps> = ({ location }) => {
 
 
-const App = () => {
-  const activityStore = useContext(ActivityStore);
+  return (
+    <Fragment>
+      <Route exact path='/' component={HomePage} />
+      <Route
+        path={'/(.+)'}
+        render={() => (
+          <Fragment>
+            <NavBar />
+            <Container style={{ marginTop: '7em' }}>
+              <Route exact path='/activities' component={ActivityDashboard} />
+              <Route path='/activities/:id' component={ActivityDetails} />
+              <Route
+                key={location.key}
+                path={['/createActivity', '/manage/:id']}
+                component={ActivityForm}
+              />
+            </Container>
+          </Fragment>
+        )}
+      />
+    </Fragment>
+  );
+};
 
-  useEffect(()=> {
-    activityStore.loadActivities();
-  }, [activityStore]);
-
-  if (activityStore.loadingInitial) return <LoadingComponent content='Loading activities...'/>
-  
-    return (
-      <Fragment>
-        <NavBar></NavBar>
-        <Container style={{marginTop: '7em'}}>
-          <ActivityDashboard />
-        </Container>
-      </Fragment>
-    );
-
-}
-
-export default observer(App);
+export default withRouter(observer(App));
--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,28 +1,27 @@
-import React, { useContext } from 'react'
-import { Menu, Container, Button } from 'semantic-ui-react'
-import ActivityStore from '../../app/stores/activityStore';
+import React from 'react';
+import { Menu, Container, Button } from 'semantic-ui-react';
 import { observer } from 'mobx-react-lite';
-
+import { NavLink } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
-    const activityStore = useContext(ActivityStore);
-    return (
-        <div>
-            <Menu fixed="top" inverted>
-                <Container>
-                    <Menu.Item header>
-                        <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}></img>
-                        Reactivities
-                    </Menu.Item>
-                    <Menu.Item name='Activities'/>
-                    <Menu.Item>
-                        <Button positive onClick={activityStore.openCreateForm} content="Create Activity" />
-                    </Menu.Item>    
-                </Container>
-                
-            </Menu>
-        </div>
-    )
-}
+  return (
+    <Menu fixed='top' inverted>
+      <Container>
+        <Menu.Item header as={NavLink} exact to='/'>
+          <img src='/assets/logo.png' alt='logo' style={{ marginRight: 10 }} />
+          Reactivities
+        </Menu.Item>
+        <Menu.Item name='Activities' as={NavLink} to='/activities' />
+        <Menu.Item>
+          <Button
+            as={NavLink} to='/createActivity'
+            positive
+            content='Create Activity'
+          />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
+};
 
-export default observer(NavBar)
+export default observer(NavBar);
